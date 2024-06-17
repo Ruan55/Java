b5beee29bc438b6fb94c1fe13225ac4ef3c41735
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-06-17 17:30:10 -0300

    Add files via upload

---
+++ PrimeirosPassos.java
@@ -0,0 +1,41 @@
+public class PrimeirosPassos {
+
+    public static void main(String[] args){
+        
+        // Chamando as funções
+        somaDeNumeros();
+        subtracaoDeNumeros();
+        multiplicacaoDeNumeros();
+        divisaoDeNumeros();
+
+    }
+
+   static void somaDeNumeros(){
+
+        // Imprimindo no terminal a soma de dois números
+        System.out.println(2 + 2);
+
+    }
+
+    static void subtracaoDeNumeros(){
+
+        // Imprimindo no terminal a subtração de dois números
+        System.out.println(2 - 2);
+
+    }
+
+    static void multiplicacaoDeNumeros(){
+
+        // Imprimindo no terminal a multiplicação de dois números
+        System.out.println(2 * 2);
+
+    }
+
+    static void divisaoDeNumeros(){
+
+        // Imprimindo no terminal a divisão de dois números
+        System.out.println(2 / 2);
+
+    }
+    
+}
