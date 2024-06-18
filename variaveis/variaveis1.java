10b265b4282ecd3f368e8e3c5904a4c2bd7b0a0e
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-06-18 18:11:41 -0300

    Add files via upload

---
+++ variaveis/variaveis1.java
@@ -0,0 +1,29 @@
+package variaveis;
+
+public class variaveis1 {
+
+    public static void main(String[] args){
+
+        // Variavel em java
+        String nome = "Ruan";
+        int idade = 21;
+        double altura = 1.80;
+        boolean masculino = true;
+
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(altura);
+        System.out.println(masculino);
+
+        /* Tipos de variáveis
+        
+        - Texto (Strings)
+        - Numeros (int)
+        - Numeros Decimais (double)
+        - Booleanos (true || false)
+        
+        */        
+
+    }
+    
+}
