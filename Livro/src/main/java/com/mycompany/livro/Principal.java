2b54954f19973ed60a2a1131102063f16b208522
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-08-07 14:29:49 -0300

    Update Principal.java
    Imprimindo as informações no terminal

--- Livro/src/main/java/com/mycompany/livro/Principal.java
+++ Livro/src/main/java/com/mycompany/livro/Principal.java
@@ -15,6 +15,14 @@ public class Principal {
     Livros livro2 = new Livros("A Arte da Guerra", "Sun Tzu", 160, 10.73);
         
         System.out.println("Titulo: " + livro1.getTitulo());
+        System.out.println("Autor: " + livro1.getAutor());
+        System.out.println("Número de páginas: " + livro1.getNumero());
+        System.out.println("Preço do livro: " + livro1.getPreco());
+
+        System.out.println("Titulo: " + livro2.getTitulo());
+        System.out.println("Autor: " + livro2.getAutor());
+        System.out.println("Número de páginas: " + livro2.getNumero());
+        System.out.println("Preço do livro: " + livro2.getPreco());
     
     }
 }
