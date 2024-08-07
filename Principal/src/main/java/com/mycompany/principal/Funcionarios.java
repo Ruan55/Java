41007caf619ccb64cb22c5cd41023c77c60168f7
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-08-07 10:47:53 -0300

    Add files via upload

---
+++ Principal/src/main/java/com/mycompany/principal/Funcionarios.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.principal;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Funcionarios {
+    
+    private String nome;
+    private int idade;
+    private double salario;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    
+    
+}
---
+++ Principal/src/main/java/com/mycompany/principal/Principal.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.principal;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+    Funcionarios funcionario = new Funcionarios();
+    
+        funcionario.setNome("Ruan");
+        funcionario.setIdade(21);
+        funcionario.setSalario(3400);
+        
+        System.out.println("Nome: " + funcionario.getNome());
+        System.out.println("Idade: " + funcionario.getIdade());
+        System.out.println("Salario: " + funcionario.getSalario());
+         
+    }
+}
